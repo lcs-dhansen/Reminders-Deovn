ec9a2ad0ae02d9efa50198f36419c4193710f196
Devon Hanses <dhansen@lcs.on.ca>
2021-03-18 08:57:37 -0400

    Initial Commit

---
+++ Shared/Reminders_DeovnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_DeovnApp.swift
+//  Shared
+//
+//  Created by Devon Hansen on 2021-03-18.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_DeovnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
